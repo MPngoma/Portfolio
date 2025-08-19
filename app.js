f59cbe2a2b7c8e1b5bf4129422ae59d05a52b361
Milton <milto@LAPTOP2021EBS.com>
2025-08-18 20:05:40 -0500

    initial portfolio

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/experience', (req, res) => res.render('experience'));
+app.get('/contact', (req, res) => res.render('contact'));
+
+app.listen(3000, () => console.log('Server started on http://localhost:3000'));
